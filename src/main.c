a340dc61deb7f811e931861aae3fd5f61db78158
VS-123 <>
2026-03-07 07:39:05 +0000

    main.c: basic process_buffer

--- src/main.c
+++ src/main.c
@@ -17,7 +17,33 @@ typedef struct
    size_t partial_cap;
 } scanner_t;
 
-void process_buffer(scanner_t *s, const char *query);
+void
+process_buffer (scanner_t *s, const char *query)
+{
+   size_t start = 0;
+
+   for (size_t i = 0; i < s->buffer_valid_bytes; i++)
+      {
+         if (s->buffer[i] == '\n')
+            {
+               start = i + 1;
+
+               /**
+                  [TODO]
+                  - combine =s->partial_line= with the segment from start to =i=
+                  - search for =query=
+                  - print when match found
+		  - reset partial_line
+	       */
+            }
+      }
+
+   /**
+      [TODO]
+      - handle reamining bytes
+      - move =s->buffer[start..valid_bytes]= to =s->partial_line=
+   */
+}
 
 int
 main (void)
