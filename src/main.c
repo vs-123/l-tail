d5dfc0fd617107576b381f0a4f7849a2cc9035e9
VS-123 <>
2026-03-07 07:35:04 +0000

    main.c: process buffer signature

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define BUF_SZ 8192
 
@@ -13,6 +16,8 @@ typedef struct
    size_t partial_len;
    size_t partial_cap;
 } scanner_t;
+
+void process_buffer(scanner_t *s, const char *query);
 
 int
 main (void)
