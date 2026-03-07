855c0de2255efa41e0d77aae7fec69f7c5f90e18
VS-123 <>
2026-03-07 07:33:31 +0000

    main.c: define scanner struct

--- src/main.c
+++ src/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+#define BUF_SZ 8192
+
+typedef struct
+{
+   int fd;
+   char buffer[BUF_SZ];
+   size_t buffer_valid_bytes;
+   size_t pos;
+
+   char *partial_line;
+   size_t partial_len;
+   size_t partial_cap;
+} scanner_t;
+
 int
 main (void)
 {
